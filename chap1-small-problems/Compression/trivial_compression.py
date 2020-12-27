f268afb521e7d07a3e41afed7ad4c6448e0c9f07
Dave Pitts <dgapitts@gmail.com>
2020-12-27 13:39:54 +0100

    added README.md covering setup for Compression/trivial_compression.py

--- chap1-small-problems/Compression/trivial_compression.py
+++ chap1-small-problems/Compression/trivial_compression.py
@@ -38,7 +38,7 @@ class CompressedGene:
 
 if __name__ == "__main__":
     from sys import getsizeof
-    original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA" * 100
+    original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA" * 3
     print("original is {} bytes".format(getsizeof(original)))
     compressed: CompressedGene = CompressedGene(original)  # compress
     print("compressed is {} bytes".format(getsizeof(compressed.bit_string)))
